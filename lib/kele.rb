b4efdc25cbce1725cd64b1f2b40b3034f8689a67
Wan Cheul Kim <wckim88@gmail.com>
2018-03-12 23:12:17 -0700

    Checkpoint-4: Retrieve list of mentor's available time slots and convert it to Ruby array

--- lib/kele.rb
+++ lib/kele.rb
@@ -21,4 +21,11 @@ class Kele
     response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
     @user_hash = JSON.parse(response.body)
   end
+
+  def get_mentor_availability(id)
+    values = { "id": id }
+
+    response = self.class.get("/mentors/#{id}/student_availability", body: values, headers: { "authorization" => @auth_token })
+    @available_time = JSON.parse(response.body)
+  end
 end
