305abf0d2a8e423a0cf2ebeed3b5f5cf13b0ebcf
Wan Cheul Kim <wckim88@gmail.com>
2018-03-12 22:40:55 -0700

    Checkpoint-3: Retrieve user data and convert it to Ruby hash

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,18 +1,24 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
+  include JSON
+
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, pw)
-
     values = {
       "email": email,
       "password": pw
     }
 
     response = self.class.post('/sessions', body: values)
-
     @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    @user_hash = JSON.parse(response.body)
   end
 end
