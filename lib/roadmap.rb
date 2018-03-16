d9288e7f70115ace079f9573b34a9d76a0e4ca7f
Wan Cheul Kim <wckim88@gmail.com>
2018-03-15 23:34:16 -0700

    Checkpoint-7: get_remaining_checkpoints method added

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -12,4 +12,9 @@ module Roadmap
     response = self.class.get("/checkpoints/#{checkpoint_id}", body: values, headers: { "authorization" => @auth_token })
     @checkpoint = JSON.parse(response.body)
   end
+
+  def get_remaining_checkpoints(chain_id)
+    response = self.class.get("/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section", headers: { "authorization" => @auth_token })
+    @remaining_checkpoints = JSON.parse(response.body)
+  end
 end
