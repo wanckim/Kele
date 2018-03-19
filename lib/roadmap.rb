5ac1fee33719ceeb6a54fff77eb712cf9daa006c
Wan Cheul Kim <wckim88@gmail.com>
2018-03-18 22:01:55 -0700

    Roadmap module revision

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,20 +1,16 @@
 module Roadmap
   def get_roadmap(chain_id)
-    values = { "id": chain_id }
-
-    response = self.class.get("/roadmaps/#{chain_id}", body: values, headers: { "authorization" => @auth_token })
-    @roadmap = JSON.parse(response.body)
+    response = self.class.get("/roadmaps/#{chain_id}", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
   end
 
   def get_checkpoint(checkpoint_id)
-    values = { "id": checkpoint_id }
-
-    response = self.class.get("/checkpoints/#{checkpoint_id}", body: values, headers: { "authorization" => @auth_token })
-    @checkpoint = JSON.parse(response.body)
+    response = self.class.get("/checkpoints/#{checkpoint_id}", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
   end
 
   def get_remaining_checkpoints(chain_id)
     response = self.class.get("/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section", headers: { "authorization" => @auth_token })
-    @remaining_checkpoints = JSON.parse(response.body)
+    JSON.parse(response.body)
   end
 end
